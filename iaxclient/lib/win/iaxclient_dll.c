c440ed3fd52d478e30fcc5634ab1d20e2b096c5c
sbalea <sbalea@f854266a-e20d-0410-a9be-8852184c940f>
2006-08-30 21:33:30 +0000

    Project files for Windows build environments (VC6, VS2003, VS2005). Submitted by Bill Welch.
    git-svn-id: https://appconference.svn.sourceforge.net/svnroot/appconference/trunk@626 f854266a-e20d-0410-a9be-8852184c940f

---
+++ iaxclient/lib/win/iaxclient_dll.c
@@ -0,0 +1,31 @@
+/*
+ * iaxclient: a cross-platform IAX softphone library
+ *
+ * Copyrights:
+ * Copyright (C) 2003 HorizonLive.com, (c) 2004, Horizon Wimba, Inc.
+ *
+ * Contributors:
+ * Steve Kann <stevek@stevek.com>
+ * Michael Van Donselaar <mvand@vandonselaar.org> 
+ * Shawn Lawrence <shawn.lawrence@terracecomm.com>
+ * Frik Strecker <frik@gatherworks.com>
+ *
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU Lesser (Library) General Public License
+ */
+
+//#define INITGUID
+//#include <initguid.h>
+#include <windows.h>
+
+BOOL APIENTRY DllMain(
+	HANDLE			hModule, 
+    DWORD			ul_reason_for_call, 
+    LPVOID			lpReserved)
+{
+    return TRUE;
+}
+
+/* #EOF# */
+
