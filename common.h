b475a87dc4de404358e0a92bf7b87045b66f0e01
sbalea <sbalea@f854266a-e20d-0410-a9be-8852184c940f>
2007-09-04 17:26:27 +0000

    Fix copyright notices n source files Update TODO
    git-svn-id: https://appconference.svn.sourceforge.net/svnroot/appconference/trunk@892 f854266a-e20d-0410-a9be-8852184c940f

--- common.h
+++ common.h
@@ -8,12 +8,24 @@
  *
  * Copyright (C) 2002, 2003 Junghanns.NET GmbH
  * Copyright (C) 2003, 2004 HorizonLive.com, Inc.
+ * Copyright (C) 2005, 2006 HorizonWimba, Inc.
+ * Copyright (C) 2007 Wimba, Inc.
  *
  * Klaus-Peter Junghanns <kapejod@ns1.jnetdns.de>
  *
- * This program may be modified and distributed under the 
- * terms of the GNU Public License.
+ * Video Conferencing support added by 
+ * Neil Stratford <neils@vipadia.com>
+ * Copyright (C) 2005, 2005 Vipadia Limited
  *
+ * VAD driven video conferencing, text message support 
+ * and miscellaneous enhancements added by 
+ * Mihai Balea <mihai at hates dot ms>
+ *
+ * This program may be modified and distributed under the 
+ * terms of the GNU General Public License. You should have received 
+ * a copy of the GNU General Public License along with this 
+ * program; if not, write to the Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #ifndef _APP_CONF_COMMON_H
--- member.c
+++ member.c
@@ -8,6 +8,8 @@
  *
  * Copyright (C) 2002, 2003 Junghanns.NET GmbH
  * Copyright (C) 2003, 2004 HorizonLive.com, Inc.
+ * Copyright (C) 2005, 2006 HorizonWimba, Inc.
+ * Copyright (C) 2007 Wimba, Inc.
  *
  * Klaus-Peter Junghanns <kapejod@ns1.jnetdns.de>
  *
@@ -15,9 +17,15 @@
  * Neil Stratford <neils@vipadia.com>
  * Copyright (C) 2005, 2005 Vipadia Limited
  *
- * This program may be modified and distributed under the 
- * terms of the GNU Public License.
+ * VAD driven video conferencing, text message support 
+ * and miscellaneous enhancements added by 
+ * Mihai Balea <mihai at hates dot ms>
  *
+ * This program may be modified and distributed under the 
+ * terms of the GNU General Public License. You should have received 
+ * a copy of the GNU General Public License along with this 
+ * program; if not, write to the Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <stdio.h>
